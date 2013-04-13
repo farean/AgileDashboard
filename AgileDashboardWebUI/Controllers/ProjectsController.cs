5e68361698013f77222bcb2fa74e142de785a75c
unknown <farean@gmail.com>
2013-04-13 18:13:07 -0300

    :x

---
+++ AgileDashboardWebUI/Controllers/ProjectsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AgileDashboardWebUI.Models;
+namespace AgileDashboardWebUI.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        //
+        // GET: /Projects/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetProjects()
+        {
+            List<Project> projects = new List<Project>();
+            JsonResult listprojects=new JsonResult();
+            listprojects.Data=projects;
+            
+            return listprojects;
+            }
+    }
+}
---
+++ AgileDashboardWebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AgileDashboardWebUI.MvcApplication" Language="C#" %>
